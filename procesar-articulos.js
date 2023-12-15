db57c429fc1458502322f1cf74aa605224c5de07
Mario Rivas Reinoso <99510900+Aquiline848@users.noreply.github.com>
2023-12-15 20:09:26 +0100

    Update procesar-articulos.js

--- procesar-articulos.js
+++ procesar-articulos.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-// Leer el archivo
+
 const data = fs.readFileSync('articles.txt', 'utf-8');
 
-// Separar las líneas y procesar
+
 const lines = data.split('\n');
 const htmlOutput = lines.map(line => {
     const match = line.match(/Art\. (\d+\.\d+) - (.*): (.+)/);
@@ -13,8 +13,8 @@ const htmlOutput = lines.map(line => {
         const sancion = match[3];
         return `<li id="art-${id.replace('.', '-')}" class="flex items-center py-2" data-sancion="${sancion.trim()}"> <span>${line.trim()}</span> </li>`;
     }
-    return "";  // Retorna una línea vacía si no hay coincidencia
+    return "";  
 }).join('\n');
 
-// Guardar el resultado en un nuevo archivo
+
 fs.writeFileSync('output.html', htmlOutput);
